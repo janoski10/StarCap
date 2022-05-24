a36472eda17545f5bcc7f82e968375afcbe17794
janoski10 <alejandro.acuna09@inacapmail.cl>
2022-05-24 15:42:44 -0400

    Clase 24-05

---
+++ StarCap/StarCapWeb/Default.aspx.cs
@@ -0,0 +1,50 @@
+﻿using StarCapModel;
+using StarCapModel.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StarCapWeb
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private IClientesDAL clientesDAL = new ClienteDALObjetos();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void agregarBtn_Click(object sender, EventArgs e)
+        {
+            //1. Obtener los datos del formulario
+            string nombre = this.nombreTxt.Text.Trim();
+            string rut = this.Rut.Text.Trim();
+            //obtener el valor del dropdown
+            string bebidaValor = this.bebidaDdl.SelectedValue;
+            //obtener el texto
+            string bebidaTexto = this.bebidaDdl.SelectedItem.Text;
+            int nivel = Convert.ToInt32(this.nivelRbl.SelectedValue);
+            //2. Construir el objeto de tipo cliente
+            Bebida bebida = new Bebida()
+            {
+                Codigo = bebidaValor,
+                Nombre = bebidaTexto
+            };
+            Cliente cliente = new Cliente()
+            {
+                Nombre = nombre,
+                Rut = rut,
+                Nivel = nivel,
+                BebidaFavorita = bebida
+            };
+            //3. Llamar al DAL
+            clientesDAL.Agregar(cliente);
+            //4. Mostrar mensaje de exito
+            this.mensajeLbl.Text = "Cliente Ingresado Exitosamente !!!";
+
+        }
+    }
+}
---
+++ StarCap/StarCapWeb/Default.aspx.designer.cs
@@ -0,0 +1,69 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace StarCapWeb {
+    
+    
+    public partial class Default {
+        
+        /// <summary>
+        /// Control mensajeLbl.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label mensajeLbl;
+        
+        /// <summary>
+        /// Control nombreTxt.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox nombreTxt;
+        
+        /// <summary>
+        /// Control Rut.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Rut;
+        
+        /// <summary>
+        /// Control bebidaDdl.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList bebidaDdl;
+        
+        /// <summary>
+        /// Control nivelRbl.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList nivelRbl;
+        
+        /// <summary>
+        /// Control agregarBtn.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button agregarBtn;
+    }
+}
